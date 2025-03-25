5c5f39ed25745696f9a42c74631bbc687056e3af
reyalo <cheetaof71@gmail.com>
2025-03-25 15:27:13 +0600

    25_03_25

--- test.c
+++ test.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include<string.h>
 
 
 
 int main() {
+    freopen("output.txt", "w", stdout);
+    // char a = 2, b = -2;
+    // unsigned char c = 2, d = -2;
 
-    char a = 2, b = -2;
-    unsigned char c = 2, d = -2;
+    // printf("%x %x\n", a & 0xff, a & 0xffff);
+    // printf("%x %x\n", b & 0xff, b & 0xffff);
+    // printf("%x %x\n", c & 0xff, c & 0xffff);
+    // printf("%x %x\n", d & 0xff, d & 0xffff);
 
-    printf("%x %x\n", a & 0xff, a & 0xffff);
-    printf("%x %x\n", b & 0xff, b & 0xffff);
-    printf("%x %x\n", c & 0xff, c & 0xffff);
-    printf("%x %x\n", d & 0xff, d & 0xffff);
+
 
     
 
-    char str1 = "JIM";
-    char str2 = {'J', 'I', 'M'};
+    // char str1 = "JIM";
+    // char str2 = {'J', 'I', 'M'};
 
-    printf("%d\n", strcmp(str2, str1));
+    // printf("%d\n", strcmp(str2, str1));
+
+    char *str = NULL;
+
+    // int d = strlen(str);
+
+    char str2[] = "ab\b\111";
+
+    printf("%d\n", sizeof(str2));
+
+    printf("\n\n%#-05o\n", 36);
+
 }
